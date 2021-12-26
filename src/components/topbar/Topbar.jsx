875e7285b88870f1f5be1246f7ae9d402d96f343
abilash <e18013@eng.pdn.ac.lk>
2021-12-26 18:45:28 +0530

    menu created

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,9 +3,9 @@ import './topbar.scss'
 import PersonIcon from '@mui/icons-material/Person';
 import MailIcon from '@mui/icons-material/Mail';
 
-const Topbar=()=>{
+const Topbar=({menuOpen,setMenuOpen})=>{
     return(
-        <div className="topbar">
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">genius.</a>
@@ -19,7 +19,7 @@ const Topbar=()=>{
                     </div>
                 </div>
                 <div className="right">
-                    <div className="hamburger">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
                         <span className="line1"></span>
                         <span className="line2"></span>
                         <span className="line3"></span>
