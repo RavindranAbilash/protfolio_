847d97a827a689c9e0f294b1e1f3b921473d7677
Abilash <e18013@eng.pdn.ac.lk>
2022-06-17 08:48:45 +0530

    Footer.jsx   created

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { social } from "../data/dummydata"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        {social.map((item) => (
+          <>
+            <a href={item.link} target="_blank"><i data-aos='zoom-in'>{item.icon}</i></a>
+          </>
+        ))}
+        <p data-aos='zoom-in'>All Right Reserved Ravindran Abilash</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
